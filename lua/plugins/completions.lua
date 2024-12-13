f900b9799c60de608039f348b0a62e6dc3744481
MirjahonMirsaidov <mirjahonac021@gmail.com>
2024-12-13 22:45:27 +0500

    copilot nvim

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,6 +10,7 @@ return {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
+            "github/copilot.vim",
         },
         config = function()
             local cmp = require("cmp")
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>[t",
+			"<cmd>Trouble diagnostics next<cr>",
+			desc = "Next item",
+		},
+		{
+			"<leader>]t",
+			"<cmd>Trouble diagnostics previous<cr>",
+			desc = "Previous item",
+		},
+	},
+}
